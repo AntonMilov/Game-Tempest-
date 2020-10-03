3bfef533ed6747b7c7209f841983dcaeecb36e5a
AntonMilov <imfm37587@gmail.com>
2020-10-03 15:12:06 +0300

    laba3

---
+++ Tempest/GameTempest.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tempest
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class GameTempest : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+
+
+        Hero newHero=new Hero();
+
+        Rectangle scrBounds; // прямоугольник для хранения размеров экрана
+        public GameTempest()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            scrBounds = new Rectangle(0, 0,
+             Window.ClientBounds.Width,
+             Window.ClientBounds.Height);
+         
+        }
+
+    
+        protected override void Initialize()
+        {
+
+            this.IsMouseVisible = true; // отображение указателя            
+            base.Initialize();
+        }
+
+     
+        protected override void LoadContent()
+        {
+           
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+         
+        
+            newHero.sprite= Content.Load<Texture2D>("HeroSprite");
+
+   
+           
+        
+
+        }
+     
+        protected override void UnloadContent()
+        {
+
+        }
+
+      
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            newHero.move(scrBounds); // перемещенеи
+       
+           
+            base.Update(gameTime);
+        }
+
+      
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(newHero.sprite, newHero.position, Color.White); //отрисовка спрайта
+  
+            base.Draw(gameTime); //отрисовка компонента 
+            spriteBatch.End();
+
+
+          
+        }
+    }
+}
---
+++ Tempest/Hero.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Microsoft.Xna.Framework.Content;
+namespace Tempest
+{
+    class Hero
+    {
+        public Texture2D sprite; // переменная для хранения спрайта
+        public Vector2 position = new Vector2(150, 200); // позиция на экране
+        public Vector2 speed = new Vector2(1, 1); // скорость перемещения 
+
+        bool is_MouseLB_Pressed = false; // устанавливается в TRUE при нажатии на левую кнопку мыши
+        Vector2 new_position = new Vector2(); // новая позиция указателя
+      
+        Rectangle sprRectangle = new Rectangle(0, 0, 61, 78);//размер героя
+        
+        public void move(Rectangle scrBounds)
+        {
+            MouseState mState = Mouse.GetState();
+            KeyboardState kbState = Keyboard.GetState();            if (kbState.IsKeyDown(Keys.LeftShift)) //увеличение скорости перемещения 
+            {
+                
+                speed = new Vector2(2, 2);
+            }
+            else
+            {
+                speed = new Vector2(1, 1);
+            }
+            if (mState.LeftButton == ButtonState.Pressed)
+            {
+                is_MouseLB_Pressed = true;
+                new_position.X = mState.X;
+                new_position.Y = mState.Y;
+            }
+           
+            if (is_MouseLB_Pressed)
+            {
+                if (new_position.X - position.X > 0|| position.X > scrBounds.Width)
+                {
+                    position.X += speed.X;
+                }
+                else if (new_position.X - position.X < 0)
+                {
+                    position.X -= speed.X;
+                }
+                if (new_position.Y - position.Y > 0)
+                {
+                    position.Y += speed.Y;
+                }
+                else if (new_position.Y - position.Y < 0)
+                {
+                    position.Y -= speed.Y;
+                }
+                if (position == new_position)
+                {
+                    is_MouseLB_Pressed = false;
+                }
+
+                // проверка на пересечение границ экрана
+                if (position.X < scrBounds.Left)
+                {
+                    position.X = scrBounds.Left;
+                    is_MouseLB_Pressed = false;
+                }
+                if (position.X > scrBounds.Width - sprRectangle.Width)
+                {
+                    position.X = scrBounds.Width - sprRectangle.Width;
+                    is_MouseLB_Pressed = false;
+                }
+                if (position.Y < scrBounds.Top)
+                {
+                    position.Y = scrBounds.Top;
+                    is_MouseLB_Pressed = false;
+                }
+                if (position.Y > scrBounds.Height - sprRectangle.Height)
+                {
+                    position.Y = scrBounds.Height - sprRectangle.Height;
+                    is_MouseLB_Pressed = false;
+                }
+
+            }
+
+        }
+      
+    }
+}
